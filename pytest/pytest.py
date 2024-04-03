9a7527fb57516ccc6761901c4fbafe9474502bdb
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-04-03 16:13:01 +0200

    Update pytest.py

--- pytest/pytest.py
+++ pytest/pytest.py
@@ -1,2 +1,37 @@
+from subprocess import Popen
+import platform, smtplib
+
+date = hora = datetime.datetime.now() 
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+
 def start_process(process):
+  Popen(process)
+
+def get_processor_name():
+  print(platform.machine())
+
+def get_processor_info():
+  print(platform.processor())
+
+def get_system():
+  print(platform.system())
+
+def get_ip():
+  print(ip)
+
+def get_hostname():
+  print(hostname)
+
+def get_date():
+  print(date)
+
+def smtp_login(email, password):
   
+
+
+
+
+
+
+
